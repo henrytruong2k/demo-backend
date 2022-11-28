e648204b6c5b82777fb8a839698e6d06dcab7f5a
huytruongquang <kzyo5962@gmail.com>
2022-11-28 21:52:43 +0700

    config redis

--- config/redis.js
+++ config/redis.js
@@ -1,15 +1,22 @@
-const redis = require('redis');
+const redis = require("redis");
+const dotenv = require("dotenv");
+dotenv.config();
 
 let redisClient;
 
 (async () => {
-    redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+  const redisURL = `redis://${process.env.REDISUSER}:${process.env.REDISPASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+  redisClient = redis.createClient({ url: redisURL });
 
-    redisClient.on("error", (error) => console.error(`Error : ${error}`));
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
 
-    redisClient.on("connect", () => console.log(`Redis connected on ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`));
+  redisClient.on("connect", () =>
+    console.log(
+      `Redis connected on ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+    )
+  );
 
-    await redisClient.connect();
+  await redisClient.connect();
 })();
 
 module.exports = redisClient;

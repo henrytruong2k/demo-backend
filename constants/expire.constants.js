843fc3e4a92dcdb0b1feb53f2e8f19b07b95bd39
Truong Quang Huy <61649070+kzyo5962@users.noreply.github.com>
2022-12-01 15:53:19 +0700

    Update expire.constants.js

--- constants/expire.constants.js
+++ constants/expire.constants.js
@@ -1,5 +1,5 @@
 const TIME_EXPIRE = 3;
-const WHITE_LIST = ["http://localhost:3000"];
+const WHITE_LIST = ["http://localhost:3000", "https://demo-frontend-azure.vercel.app"];
 
 module.exports = {
   TIME_EXPIRE,
